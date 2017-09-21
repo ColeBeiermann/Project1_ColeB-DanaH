c9535c9036184ef1c2fdcd6430120ab7a35bacee
ColeBeiermann <31995657+ColeBeiermann@users.noreply.github.com>
2017-09-21 13:40:30 -0500

    Add files via upload

--- phaseII.java
+++ phaseII.java
@@ -61,9 +61,9 @@ public class phaseII {
 						int zip = Integer.parseInt(addressTokens[3]);
 						String country = addressTokens[4];
 					Address address0 = new Address(street, city, state, zip, country);
-					String email[] = Ptokens[3].spllt(",");
+					String email[] = Ptokens[3].split(",");
 					
-					person0 = new Person(lastName, firstName, street, address0);
+					person0 = new Person(lastName, firstName, address0, email[]);
 					
 					result.add(person0);
 				}
@@ -76,10 +76,59 @@ public class phaseII {
 	
 	
 	public static List<Product> parseDataFile() {
+		List<Product> result = new ArrayList<Product>();
+		try {
+			File f = new File("data/Products.dat");
+			Scanner s = new Scanner(f);
+			while(s.hasNext()) {
+				String line = s.nextLine();
+				if(!line.trim().isEmpty()) {
+					Product product0 = null;
+					String Ptokens[] = line.split(";");
+					String productCode = Ptokens[0];
+					String productType = Ptokens[1];
+					if(productType == "M") {
+						String dateTime = Ptokens[2];
+						String name = Ptokens[3];
+						String addressTokens[] = Ptokens[4].split(",");
+							String street = addressTokens[0];
+							String city = addressTokens[1];
+							String state = addressTokens[2];
+							int zip = Integer.parseInt(addressTokens[3]);
+							String country = addressTokens[4];
+						Address address0 = new Address(street, city, state, zip, country);
+						String screenNo = Ptokens[5];
+						double pricePerUnit = Double.parseDouble(Ptokens[6]);
+					}
+					else if(productType == "S") {
+						String name = Ptokens[2];
+						String startDate = Ptokens[3];
+						String endDate = Ptokens[4];
+						double cost = Double.parseDouble(Ptokens[5]);
+					}
+					else if(productType == "P") {
+						double parkingFee = Double.parseDouble(Ptokens[2]);
+					}
+					else if(productType == "R") {
+						String name = Ptokens[2];
+						double cost = Double.parseDouble(Ptokens[3]);
+					}
+					
+					product0 = new Person(productType, name, address0, screenNo, pricePerUnit, dateTime, startDate, endDate, cost);
 		
-	//Add "products" code
+					result.add(product0);
+				}
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+		
 		
 	}
+	
+	
+	//Fix issues in code. Need proper class files.
 	
 	
 	public static void main(String args[]) {
@@ -89,9 +138,7 @@ public class phaseII {
 	}
 	
 	
-	
-	
-	
+
 	
 	}
 
